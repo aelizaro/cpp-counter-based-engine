ecd7634a2e70808a994ef584c92511f108fe427b
Elizarova, Alina <alina.elizarova@intel.com>
2024-02-09 06:33:03 -0800

    add philox4x64

--- alternative_version/canonical_value_check.cpp
+++ alternative_version/canonical_value_check.cpp
@@ -1,17 +1,22 @@
-// clang++ -std=gnu++2a -Wall  test.cpp
-
 #include <iostream>
 #include "philox_engine.hpp"
 
 int main() {
     philox4x32 engine;
+    philox4x64 engine64;
 
     for (int i = 0; i < 9999; i++) {
         std::cout << engine() << std::endl;
+        std::cout << engine64() << std::endl;
     }
 
-    std::cout << "10000th element is " << engine() << std::endl;
+    std::cout << "10000th element of philox4x32 is " << engine() << std::endl;
 
     std::cout << "with a default seed " << philox4x32::default_seed << std::endl;
+
+
+    std::cout << "10000th element of philox4x64 is " << engine64() << std::endl;
+
+    std::cout << "with a default seed " << philox4x64::default_seed << std::endl;
     return 0;
 }
